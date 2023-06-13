4730553a59db70728185b9c871a4cc94981044cd
Orhanyil <yildirimorhan4790@gmail.com>
2023-06-13 14:35:49 +0200

    deleteproducts

--- 004-Checkout-App/app.js
+++ 004-Checkout-App/app.js
@@ -1,73 +1,6 @@
 //**********variables
 
-
-//**********selector
-
-
-//const main = document.querySelector(".main__product")4
-
-const main = document.getElementById("products-preview")
-
-const amountOfProducts = document.querySelector(".amount-of-product")
-
-const selectedProducts = document.querySelector(".main__sum-price")
-
-const shipping = document.getElementById("cart-shipping").querySelector(".dollar")
-const tax = document.getElementById("cart-tax").querySelector(".dollar")
-const total = document.getElementById("cart-total").querySelector(".dollar")
-const totalValue = document.getElementById("cart-total").querySelector(".dollar")
-
-
-window.addEventListener("load",() => {
-    selectedProducts.innerHTML = updateAmount();
-    finishOrder()
-})
-
-main.addEventListener("click", (e) => {
-
-    console.log(e.target.className);
-
-    if(e.target.classList.contains("fa-minus")) {
-
-        const productSubtotal = e.target.closest(".main__product-info");
-
-        if(e.target.nextElementSibling.innerText > 1) {
-            e.target.nextElementSibling.innerText --;
-            
-            productSubtotal.querySelector(".main__product-line-price").innerText = (e.target.nextElementSibling.innerText * productSubtotal.querySelector(".dollar").innerText).toFixed(2)
-        }
-        
-    } 
-
-    else if(e.target.classList.contains("fa-plus")) {
-
-        const productSubtotal = e.target.closest(".main__product-info");
-        
-        if(e.target.previousElementSibling.innerText > 0) {
-            e.target.previousElementSibling.innerText ++;
-
-            productSubtotal.querySelector(".main__product-line-price").innerText = (e.target.previousElementSibling.innerText * productSubtotal.querySelector(".dollar").innerText).toFixed(2)
-
-        }
-           
-    } 
-
-    else if (e.target.classList.contains("fa-trash-can")) {
-        const productRow = e.target.closest(".main__product");
-        if (confirm("Product removed from cart.") == true) {
-          productRow.remove();
-
-          amountOfProducts.innerText --
-        }
-        else {}
-      }
-
-      selectedProducts.innerHTML = updateAmount()
-
-      finishOrder()
-
-   
-})
+//************FUNCTIONS */
 
 function updateAmount () {
     let totalAmount = document.getElementsByClassName("main__product-line-price");
@@ -81,8 +14,72 @@ function updateAmount () {
 }
 
 function finishOrder() {
-    const selectedProductsValue = Number(selectedProducts.innerText);
-    const shippingValue = (selectedProductsValue >= 3000) ? "0" : "25.99";
-    const taxValue = (selectedProductsValue * 0.18).toFixed(2);
-    const totalValue = (Number(shippingValue) + Number(taxValue) + selectedProductsValue).toFixed(2);
+    (Number(selectedProducts.innerText) >= 3000) ? shipping.innerText = "0" : shipping.innerText = "25.99";
+    tax.innerText = (Number(selectedProducts.innerText) * 0.18).toFixed(2);
+    total.innerText = (Number(shipping.innerText) + Number(tax.innerText) + Number(selectedProducts.innerText)).toFixed(2);
+}
+
+
+
+//**********selector
+
+
+//const main = document.querySelector(".main__product")4
+
+const main = document.getElementById("products-preview")
+const nav = document.querySelector("nav")
+const amountOfProducts = document.querySelector(".amount-of-product")
+const selectedProducts = document.querySelector(".main__sum-price")
+const shipping = document.getElementById("cart-shipping").querySelector(".dollar")
+const tax = document.getElementById("cart-tax").querySelector(".dollar")
+const total = document.getElementById("cart-total").querySelector(".dollar")
+// ******** events
+window.addEventListener("load",(e) =>{
+    selectedProducts.innerHTML = updateAmount()
+    finishOreder()
+})
+main.addEventListener("click", (e) => {
+    if(e.target.classList.contains("fa-minus")) {
+        if(e.target.nextElementSibling.innerText > 1) {
+            e.target.nextElementSibling.innerText --
+            e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.nextElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
+        }
+    } else if (e.target.classList.contains("fa-plus")) {
+        e.target.previousElementSibling.innerText ++;
+        e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.previousElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
+    } else if (e.target.classList.contains("fa-trash-can")){
+        if (confirm("are you sure???") == true){
+       e.target.closest(".main__product").remove();
+       amountOfProducts.innerText --
+    }else {}
+    }
+    selectedProducts.innerHTML = updateAmount()
+    finishOreder()
+})
+nav.addEventListener("click",(e) => {
+    if (e.target.classList.contains("fa-trash-can")){
+        document.querySelectorAll(".main__product");
+        for(let i of document.querySelectorAll(".main__product") ){
+            i.remove();
+            selectedProducts.innerText = updateAmount()
+            finishOreder();
+            amountOfProducts.innerText --;
+       }
+    }
+})
+// function subTotal(position) {
+//     e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.{position}.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
+// }
+function updateAmount () {
+    let totalAmount = document.getElementsByClassName("main__product-line-price");
+    let sum = 0;
+    for(let i of totalAmount){
+         sum += +(i.innerText);
+    }
+    return sum.toFixed(2);
+}
+function finishOreder() {
+    (Number(selectedProducts.innerText) >= 3000) || (Number(selectedProducts.innerText) == 0) ? shipping.innerText = "0" : shipping.innerText = "25.99";
+    tax.innerText = (Number(selectedProducts.innerText) * 0.18).toFixed(2);
+    total.innerText = (Number(shipping.innerText) + Number(tax.innerText) + Number(selectedProducts.innerText)).toFixed(2);
 }
