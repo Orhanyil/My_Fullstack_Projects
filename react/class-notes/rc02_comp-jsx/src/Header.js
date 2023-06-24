18ddfacefd09c04a388c559bcd3fe879e9d53308
Orhanyil <yildirimorhan4790@gmail.com>
2023-06-24 11:30:10 +0200

    react

---
+++ react/class-notes/rc02_comp-jsx/src/Header.js
@@ -0,0 +1,18 @@
+
+//* Harici bir dosya icerisinde olusturulan bir component
+//* export default ile proje icerisinde herhangi bir yerde
+//* kullanima acilmis olur. Kullanmak icin hedef
+//* dosyada import .... from "./..." ile import yapmak yeterlidir.
+
+function Header() {
+  //? JS
+  return (
+    <header>
+      {/*  JSX */}
+      <h1>Header</h1>
+      <p>CW</p>
+    </header>
+  )
+}
+
+export default Header
