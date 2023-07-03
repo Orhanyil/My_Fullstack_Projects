4ccfab3c86147150fe6a273b82525a6a8139844b
Orhanyil <yildirimorhan4790@gmail.com>
2023-07-03 22:12:07 +0200

    react new

--- react/class-notes/rc02_comp-jsx/src/App.js
+++ react/class-notes/rc02_comp-jsx/src/App.js
@@ -1,12 +1,22 @@
+import Content from "./Content"
 import { Footer } from "./Footer"
 import Header from "./Header"
+import "./App.css"
 
 function App() {
+  const newStyle = {
+    fontFamily: "tohamo",
+    fontSize: "1.1rem",
+    color: "red",
+  }
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1 style={newStyle}>Hello React</h1>
       <Header/>
       <Footer/>
+      <Content/>
+
+      <h3>BU BIR H3 ELEMENTTIR</h3>
     </div>
   )
 }
--- react/class-notes/rc02_comp-jsx/src/Content.jsx
+++ react/class-notes/rc02_comp-jsx/src/Content.jsx
@@ -1,7 +1,13 @@
 const Content = () => {
     return (
-        <main>
-            
-        </main>
+      <main>
+        <h2>REACT JS</h2>
+        <p>React is JS library</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, illum.
+        </p>
+      </main>
     )
-}
+  }
+  
+  export default Content
