e519467f5c9b5dbcd1a1f4edea6af2dc4cbb9317
Orhanyil <yildirimorhan4790@gmail.com>
2023-07-06 20:52:08 +0200

    events

---
+++ react/class-notes/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+const KeyboardClipboard = () => {
+
+  const [content, setContent] = useState("")
+
+  return (
+    <div className="container mt-4">
+      <h2 className="text-center">KeyboardClipboard</h2>
+
+      <input type="text" className="form-control" onChange={(e) => setContent(e.target.value)}/>
+
+      <div className="mt-4">
+        <h3>Copied Content</h3>
+        <p>{content}</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default KeyboardClipboard
