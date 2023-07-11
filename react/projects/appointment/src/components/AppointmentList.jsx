73e085942f8cabca96e290eb12da99b94437bd99
Orhanyil <yildirimorhan4790@gmail.com>
2023-07-11 22:56:51 +0200

    doctors

--- react/projects/appointment/src/components/AppointmentList.jsx
+++ react/projects/appointment/src/components/AppointmentList.jsx
@@ -1,12 +1,29 @@
 import Stack from 'react-bootstrap/Stack';
 
-const AppointmentList = () => {
+
+const AppointmentList = ({ id, day, patient, consulted, doctor }) => {
   return (
-    <div><Stack direction="horizontal" gap={3}>
-    <div className="p-2">First item</div>
-    <div className="p-2 ms-auto">Second item</div>
-    <div className="p-2">Third item</div>
-  </Stack></div>
+    
+    <div>
+                
+        <Stack direction="horizontal" className='d-flex justify-content-around bg-secondary text-light mb-2 mx-2' gap={3}>
+            <div>
+                <div className="p-2">Patient Name</div>
+                <div className="p-2">Docter Name</div>
+            </div>
+            <div>
+                <div className="p-2 ms-auto">Consulted</div>
+                <div className="p-2 ms-auto">date</div>
+                <div className="p-2 ms-auto">time</div>
+            </div>
+            <div>
+                <div className="p-2">Delete icon</div>
+            </div>
+            
+            
+        </Stack>
+        
+  </div>
   )
 }
 
