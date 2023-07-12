1235300f3732654caacc2fa6ff359e0de00ec3f4
Orhanyil <yildirimorhan4790@gmail.com>
2023-07-12 23:05:00 +0200

    fix: cosulted

--- react/class-notes/rc08_appointment-app/appoinment-app-inClass/src/components/AddModal.jsx
+++ react/class-notes/rc08_appointment-app/appoinment-app-inClass/src/components/AddModal.jsx
@@ -1,42 +1,68 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
+import Form from "react-bootstrap/Form"
+import { useState } from "react"
 
-function AddModal({show, handleClose}) {
-  
+function AddModal({ show, handleClose, apps, setApps, drName }) {
+  const [name, setName] = useState("")
+  const [date, setDate] = useState(new Date().toISOString().slice(0, 10))
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setApps([
+      ...apps,
+      {
+        id: new Date().getTime,
+        patient: name,
+        day: date,
+        consulted: false,
+        doctor: drName,
+      }
+    ])
+
+    handleClose()
+  }
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Appointment for ...</Modal.Title>
+          <Modal.Title>Appointment for {drName}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Patient Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter Name" />
-       </Form.Group>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Date</Form.Label>
-        <Form.Control type="date" placeholder="Date" />
-      </Form.Group>
-      <div className='text-center'>
-      <Button variant="success" type="submit">
-        Save
-      </Button>
-      <Button variant="danger" onClick={handleClose}>
-            Close
-          </Button>
-      </div>
-      
-            </Form>
+            <Form.Group className="mb-3" controlId="date">
+              <Form.Label>Date</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder="Date"
+                onChange={(e) => setDate(e.target.value)}
+                value={date}
+              />
+            </Form.Group>
+
+            <div className="text-center">
+              <Button variant="success" type="submit" className="me-2">
+                Save
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
+          </Form>
         </Modal.Body>
-    </Modal>
+      </Modal>
     </>
-  );
+  )
 }
 
-export default AddModal;
+export default AddModal
--- react/class-notes/rc08_appointment-app/appoinment-app-inClass/src/components/Doctors.jsx
+++ react/class-notes/rc08_appointment-app/appoinment-app-inClass/src/components/Doctors.jsx
@@ -1,34 +1,47 @@
 import Container from "react-bootstrap/Container"
-import {doctorData} from "../helper/data"
+import { doctorData } from "../helper/data"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import AddModal from "./AddModal"
 import { useState } from "react"
 
-const Doctors = () => {
+const Doctors = ({ apps, setApps }) => {
+  const [show, setShow] = useState(false)
+  const [drName, setDrName] = useState("")
 
-  const [show, setShow] = useState(false);
-
-  
-  const handleShow = () => setShow(true);
-
-  const handleImgClick= () => {handleShow(true)}
-
+  // const handleImgClick = () => {
+  //   setShow(true)
+  // }
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
-      <Row>
-      {doctorData.map(({id, img, dep, name})=> (
-        <Col xs={12} sm={6} md={4} lg={3} key={id}>
-        <img src={img} alt="img" className="img-thumbnail doctor-img" onClick={handleImgClick}/>
-        <h5>{name}</h5>
-        <h6>{dep}</h6>
-        </Col>
-      ))}
+
+      <Row className="justify-content-center">
+        {doctorData.map(({ id, img, dep, name }) => (
+          <Col xs={6} sm={4} md={3} key={id}>
+            <img
+              src={img}
+              alt={name}
+              className="img-thumbnail doctor-img"
+              onClick={() => {
+                setDrName(name)
+                setShow(true)
+              }}
+            />
+            <h5>{name}</h5>
+            <h6>{dep}</h6>
+          </Col>
+        ))}
       </Row>
-    <AddModal show={show} handleClose={()=> setShow(false)}/>
+      <AddModal
+        show={show}
+        handleClose={() => setShow(false)}
+        apps={apps}
+        setApps={setApps}
+        drName={drName}
+      />
     </Container>
   )
 }
