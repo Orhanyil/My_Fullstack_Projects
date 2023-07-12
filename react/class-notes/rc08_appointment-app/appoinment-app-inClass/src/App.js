eba8833784fe3083b8d869c4679fe287e11523e4
Orhanyil <yildirimorhan4790@gmail.com>
2023-07-12 20:07:56 +0200

    appointmen new

---
+++ react/class-notes/rc08_appointment-app/appoinment-app-inClass/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import Home from "./pages/Home"
+import {AiFillDelete} from 'react-icons/ai'
+
+const App = () => {
+  return( 
+  <div>
+    <Home />
+  
+  </div> 
+  )
+}
+
+export default App
