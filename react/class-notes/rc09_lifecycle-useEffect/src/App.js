3949483eae7b49acf6e4579998b8d77bd44f8fe4
Orhanyil <yildirimorhan4790@gmail.com>
2023-07-13 20:52:41 +0200

    feat: useEffect added

---
+++ react/class-notes/rc09_lifecycle-useEffect/src/App.js
@@ -0,0 +1,20 @@
+// import LifeCycleMethods from "./components/LifeCycleMethods"
+import { useState } from "react"
+import UseEffectHook from "./components/UseEffectHook"
+
+function App() {
+  const [visible, setVisible] = useState(false)
+  return (
+    <div className="container text-center mt-4">
+      <button className="btn btn-danger" onClick={() => setVisible(!visible)}>
+        Show/Hide
+      </button>
+
+      {/* {visible && <LifeCycleMethods />} */}
+
+      {visible && <UseEffectHook />}
+    </div>
+  )
+}
+
+export default App
