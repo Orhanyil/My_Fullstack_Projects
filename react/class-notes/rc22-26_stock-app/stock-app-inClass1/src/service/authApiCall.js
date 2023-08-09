712efc0c0303266bc69329f0eacfa5bb20ecc8a6
Orhanyil <yildirimorhan4790@gmail.com>
2023-08-09 22:37:09 +0200

    feat: add authApiCall

---
+++ react/class-notes/rc22-26_stock-app/stock-app-inClass1/src/service/authApiCall.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+import {toastErrorNotify, toastSuccessNotify} from ""
+
+export const login = async() => {
+    const BASE_URL ="https://14103.fullstack.clarusway.com/"
+    try {
+     const {data} = await axios.post(`${BASE_URL}/account/auth/login/`, userData)
+        console.log(data)
+        toastSuccessNotify("Login is success")
+    } catch (error) {
+        console.log(error)
+    }
+
+}
