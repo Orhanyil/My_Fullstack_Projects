02da5350275360d40ca098c3059f3ac01289d4f7
Orhanyil <yildirimorhan4790@gmail.com>
2023-08-09 16:46:31 +0200

    feat: add new component

--- react/class-notes/rc15-16_clarus-library/inclass/src/components/header/Header.style.jsx
+++ react/class-notes/rc15-16_clarus-library/inclass/src/components/header/Header.style.jsx
@@ -6,16 +6,15 @@ export const HeaderContainer = styled(Flex)`
   flex-direction: column;
 `;
 export const HeaderTitle = styled.h1`
-    margin-top: 1rem;
-    font-size: 3rem;
-    color: ${({theme})=> theme.colors.detailColor};
-    @media (max-width: ${({theme})=> theme.screens.lg}){
-        font-size: 1.6rem;
-    }
-
+  margin-top: 1rem;
+  font-size: 3rem;
+  color: ${({ theme }) => theme.colors.detailColor};
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    font-size: 1.6rem;
+  }
 `;
 export const HeaderForm = styled.form`
-    display: flex;
+  display: flex;
   width: 45%;
   flex-wrap: wrap;
   justify-content: center;
@@ -24,11 +23,16 @@ export const HeaderForm = styled.form`
   backdrop-filter: blur(8.3px);
   border: 1px solid rgba(15, 63, 42, 0.3);
   margin: 20px;
+  background-color: ${({ theme }) => theme.colors.headerFormColor};
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    flex-direction: column;
+    width: 90%;
+    padding: 1rem;
+  }
 `;
 
 export const SearchInput = styled.input`
-
-height: 3rem;
+  height: 3rem;
   width: 55%;
   border: none;
   margin-top: 2rem;
@@ -39,13 +43,14 @@ height: 3rem;
   border-top-left-radius: 1rem;
   border-bottom-left-radius: 1rem;
   background-color: rgb(255, 255, 255, 0.5);
-  @media (max-width: ${({theme})=> theme.screens.lg}){
-        font-size: 1.6rem;
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    width: 100%;
+    border-radius: 5px;
   }
-        `;
+`;
 
 export const SelectBox = styled.select`
-    border-radius: 3px;
+  border-radius: 3px;
   margin-top: 2rem;
   padding: 5px;
   height: 3rem;
@@ -60,11 +65,10 @@ export const SelectBox = styled.select`
     width: 100%;
     border-radius: 5px;
   }
-
 `;
 
 export const HeaderButton = styled.button`
-background-color: #e1f1dd;
+  background-color: #e1f1dd;
   padding: 5px;
   outline: none;
   height: 3rem;
@@ -77,4 +81,5 @@ background-color: #e1f1dd;
   border-bottom-right-radius: 1rem;
   :hover {
     background-color: ${({ theme }) => theme.colors.navbarBgColor};
+  }
 `;
